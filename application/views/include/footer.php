d76440949e2bd28c4f3409e0216a208d0787e0ca
Sunny <sunny@sunny-virtual-machine.(none)>
2012-12-19 20:53:19 -0500

    Added metro ui css,js,icons and images

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -1,2 +1,14 @@
 </body>
+    <script type="text/javascript" src="<?php echo base_url('assets/js/jquery-1.8.3.min.js')?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('assets/js/metro/dropdown.js')?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('assets/js/metro/accordion.js')?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('assets/js/metro/buttonset.js')?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('assets/js/metro/carousel.js')?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('assets/js/metro/input-control.js')?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('assets/js/metro/pagecontrol.js')?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('assets/js/metro/rating.js')?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('assets/js/metro/slider.js')?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('assets/js/metro/tile-drag.js')?>"></script>
+    <script type="text/javascript" src="<?php echo base_url('assets/js/metro/tile-slider.js')?>"></script>
+
 </html>
