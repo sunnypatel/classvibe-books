160a38359386c22106e43d21608c1fb09e0a6d27
Sunny <sunny@sunny-virtual-machine.(none)>
2012-12-21 11:35:54 -0500

    added IonAuth Library, updated homepage to ver1.0 mockup

--- application/views/frontpage.php
+++ application/views/frontpage.php
@@ -1,25 +1,61 @@
 <div class="page">
 		<div class="grid homepage">
-		<div class="row">
-		<div class="span6">
-			<h1 class="classvibe_title_large fg-color-white">ClassVibe<small>BookStores</small><h1>
-		</div>
-		<div class="span6">
-			<form action="google.com" method="post">
+			<div class="row">
+			<div class="span6">
+				<span class="classvibe_title_large fg-color-black">ClassVibe<small>BookStores</small></span>
+			</div>
+			<div class="span6">
+				<form action="google.com" method="post">
+					<div class="input-control text">
+						<input type="text" tabindex="1">
+						<button class="btn-search"></button>
+					</div>
+				</form>
+			</div>
+			</div>
+
+			<div class="row">
+			<div class="span12 slideshow">
+			The Slideshow
+			</div>
+			</div>
+
+			<div class="row">
+			<div class="span6 signup">
+			<h2 class="fg-color-white">signup</h2>
+			<form action="#" method="post">
 				<div class="input-control text">
-					<input type="text" placeholder="Email" tabindex="1">
-					<button class="helper"></button>
+					<input type="text" tabindex="2">
+					<button class="helper" onclick="return false"></button>
 				</div>
 
 				<div class="input-control password">
-					<input type="password" tabindex="2">
+					<input type="password">
 					<button class="helper"></button>
 				</div>
-				<span style="float:right">
-					<input class="bg-color-white fg-color-blueDark" type="submit" value="Submit" tabindex="3">
-				</span>
+				<input type="submit" class="button bg-color-white fg-color-blue" value="signup">
+
 			</form>
+			</div>
+
+			<div class="span6 login">
+				<h2 class="fg-color-white">login</h2>
+	            <form action="#" method="post">
+                <div class="input-control text">
+                    <input type="text" tabindex="2">
+                    <button class="helper" onclick="return false"></button>
+                </div>
+
+                <div class="input-control password">
+                    <input type="password">
+                    <button class="helper"></button>
+                </div>
+					<input type="submit" class="button bg-color-white fg-color-green" value="login">
+    	        </form>
+
+			</div>
+			</div>
 		</div>
-		</div>
-		</div>
+
+
 </div>
