b808d91259c6a28c0e078f3f15383eb2cbe0135c
matt <matt@ubuntu>
2013-01-08 21:13:50 -0500

    jquery validation works, moved jquery tag to header. Needs to be called first.

--- application/views/include/footer.php
+++ application/views/include/footer.php
@@ -1,6 +1,5 @@
 </body>
-    <script type="text/javascript" src="<?php echo base_url('assets/js/jquery-1.8.3.min.js')?>"></script>
-
+  
 <script type="text/javascript" src="<?php echo base_url('assets/js/bootstrap.min.js')?>"></script>
 
 </html>
